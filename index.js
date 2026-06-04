5fa1baf95cced036da8c20a687a618632e87f174
freefire448768676 <freefire4487686767@gmail.com>
2026-06-04 15:56:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { Telegraf } from "telegraf";
-import { registerAdmin, registerAdminTextHandlers } from "./workspace/admin.js";
+import { registerAdmin, registerAdminTextHandlers, startPingScheduler } from "./workspace/admin.js";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 registerAdmin(bot);
 registerAdminTextHandlers(bot);
+startPingScheduler(bot); // ضفت هاد السطر
 
 bot.launch();
-console.log('Bot started');
+console.log('تم تشغيل البوت. وحدة التحكم');
