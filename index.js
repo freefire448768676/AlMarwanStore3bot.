05922c7093ada89218c08ce90961387f4842698e
freefire448768676 <freefire4487686767@gmail.com>
2026-06-04 15:43:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { Telegraf } from "telegraf";
+import { registerAdmin, registerAdminTextHandlers } from "./workspace/admin.js";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('✅ البوت شغال'));
-bot.command('admin', (ctx) => ctx.reply('ارسل كلمة المرور'));
+registerAdmin(bot);
+registerAdminTextHandlers(bot);
 
 bot.launch();
-console.log('Bot started successfully');
+console.log('Bot started');
